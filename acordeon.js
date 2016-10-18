a589d8e7d89043d6d53fb11ff8197208f33cef76
fjpacymper <fjpa@cymper.com>
2016-10-18 17:40:14 +0100

    Create acordeon.js

---
+++ acordeon.js
@@ -0,0 +1,5 @@
+// Configuramos la librería jQuery Accordion
+$('.accordion').accordion({
+  transitionSpeed: 400,        // velocidad del acordeón
+  transitionEasing: 'ease'    // ritmo del acordeón (linear, ease-in…)
+});
