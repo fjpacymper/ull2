efefb4640ff0eca72effc6b4e12c8fa020484f73
fjpacymper <fjpa@cymper.com>
2016-10-18 19:08:13 +0100

    Create avisos.js

---
+++ avisos.js
@@ -0,0 +1,5 @@
+// Configuramos la librería Ouibounce
+ouibounce(document.getElementById('modal-card'), {
+  aggressive: true,            // borra la cookie viewedOuibounceModal
+  timer: 5                        // espera (evita falsos positivos)
+});
